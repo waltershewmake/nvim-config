1049000ee8d22a8b5f777e2a07d6391542cca85b
Walter Shewmake <waltershewmake@gmail.com>
2024-07-19 15:55:57 -0400

    feat: use eslint if config present

--- lua/walter/plugins/formatting.lua
+++ lua/walter/plugins/formatting.lua
@@ -1,39 +1,70 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local conform = require("conform")
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
 
-    conform.setup({
-      formatters_by_ft = {
-        javascript = { "biomejs" },
-        typescript = { "biomejs" },
-        javascriptreact = { "biomejs" },
-        typescriptreact = { "biomejs" },
-        svelte = { "biomejs" },
-        css = { "biomejs" },
-        html = { "biomejs" },
-        json = { "biomejs" },
-        yaml = { "biomejs" },
-        markdown = { "biomejs" },
-        graphql = { "biomejs" },
-        liquid = { "biomejs" },
-        lua = { "stylua" },
-        python = { "isort", "black" },
-      },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      },
-    })
+		local function has_file(filename)
+			local f = io.open(filename, "r")
+			if f ~= nil then
+				io.close(f)
+				return true
+			else
+				return false
+			end
+		end
 
-    vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-      conform.format({
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      })
-    end, { desc = "Format file or range (in visual mode)" })
-  end,
+		local formatters_by_ft = {
+			javascript = { "biomejs" },
+			typescript = { "biomejs" },
+			javascriptreact = { "biomejs" },
+			typescriptreact = { "biomejs" },
+			svelte = { "biomejs" },
+			css = { "biomejs" },
+			html = { "biomejs" },
+			json = { "biomejs" },
+			yaml = { "biomejs" },
+			markdown = { "biomejs" },
+			graphql = { "biomejs" },
+			liquid = { "biomejs" },
+			lua = { "stylua" },
+			python = { "isort", "black" },
+		}
+
+		if has_file(".eslintrc.cjs") or has_file(".eslintrc.json") then
+			formatters_by_ft = {
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				svelte = { "prettier" },
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				graphql = { "prettier" },
+				liquid = { "prettier" },
+				lua = { "stylua" },
+				python = { "isort", "black" },
+			}
+		end
+
+		conform.setup({
+			formatters_by_ft = formatters_by_ft,
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			},
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
 }
--- lua/walter/plugins/linting.lua
+++ lua/walter/plugins/linting.lua
@@ -1,29 +1,52 @@
 return {
-  "mfussenegger/nvim-lint",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local lint = require("lint")
+	"mfussenegger/nvim-lint",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local lint = require("lint")
 
-    lint.linters_by_ft = {
-      javascript = { "biomejs" },
-      typescript = { "biomejs" },
-      javascriptreact = { "biomejs" },
-      typescriptreact = { "biomejs" },
-      svelte = { "biomejs" },
-      python = { "pylint" },
-    }
+		local function has_file(filename)
+			local f = io.open(filename, "r")
+			if f ~= nil then
+				io.close(f)
+				return true
+			else
+				return false
+			end
+		end
 
-    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+		local linters_by_ft = {
+			javascript = { "biomejs" },
+			typescript = { "biomejs" },
+			javascriptreact = { "biomejs" },
+			typescriptreact = { "biomejs" },
+			svelte = { "biomejs" },
+			python = { "pylint" },
+		}
 
-    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-      group = lint_augroup,
-      callback = function()
-        lint.try_lint()
-      end,
-    })
+		if has_file(".eslintrc.cjs") or has_file(".eslintrc.json") then
+			linters_by_ft = {
+				javascript = { "eslint" },
+				typescript = { "eslint" },
+				javascriptreact = { "eslint" },
+				typescriptreact = { "eslint" },
+				svelte = { "eslint" },
+				python = { "pylint" },
+			}
+		end
 
-    vim.keymap.set("n", "<leader>l", function()
-      lint.try_lint()
-    end, { desc = "Trigger linting for current file" })
-  end,
+		lint.linters_by_ft = linters_by_ft
+
+		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+			group = lint_augroup,
+			callback = function()
+				lint.try_lint()
+			end,
+		})
+
+		vim.keymap.set("n", "<leader>l", function()
+			lint.try_lint()
+		end, { desc = "Trigger linting for current file" })
+	end,
 }
